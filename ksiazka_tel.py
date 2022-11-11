2f6b01482a3fb545eb71b253ccade3ad58b8a7ba
kzakrzewski1 <117981650+kzakrzewski1@users.noreply.github.com>
2022-11-11 12:01:24 +0100

    Zadanie 3

--- ksiazka_tel.py
+++ ksiazka_tel.py
@@ -1,12 +1,45 @@
-tel = {}
+p_book = {}
+numbers = set()
 
-
-
-def dodaj_numer(imie,nazwisko,typ,numer):
-    if (imie,nazwisko) in tel:
-        tel[(imie,nazwisko)]=[(typ,numer)]
+def add_number(f_name,l_name,type,number):
+    if number in numbers:
+        print("Number already in use\n")
+        return
     else:
-        tel[(imie,nazwisko)].append((typ,numer))
+        if (f_name,l_name) in p_book:
+            p_book[(f_name,l_name)].append((type, number))
+        else:
+            p_book[(f_name,l_name)] = [((type, number))]
+        numbers.add(number)
 
 
-dodaj_numer("Jan","Kowalski", "praca", "123-456-789")
+def retrieve_all(f_name,l_name):
+    print(f"{f_name} {l_name}: {p_book[(f_name,l_name)]}\n")
+
+def retrieve_type(f_name,l_name,type):
+    print(f"{f_name} {l_name} ({type}): ", end="") 
+    for item in p_book[(f_name,l_name)]:
+        if item[0] == type:
+            print(f"{item[1]}; ", end="")
+    print("\n")      
+
+
+def del_number(f_name, l_name, which):    #Podajemy, który numer chcemy usunąć (liczymy od 1)
+    p_book[(f_name,l_name)].pop(which-1)
+
+add_number("Jan","Kowalski","main","123-456-789")
+add_number("Jan","Kowalski","work","231-564-897")
+add_number("Andrzej","Nowak","work","523-532-352")
+add_number("Andrzej","Nowak","work","321-654-987")
+
+print()
+print(f"{p_book} \n")
+
+add_number("Andrzej","Nowak","main","123-456-789")
+
+retrieve_all("Jan", "Kowalski")
+retrieve_type("Andrzej", "Nowak","work")
+
+del_number("Jan", "Kowalski",2)
+
+print(f"{p_book} \n")
